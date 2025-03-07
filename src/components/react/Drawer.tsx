6d63a5e828aa72f8f4b5b1275b632815097e3dd4
Antonio Lentini <antonio.lentini2004@gmail.com>
2025-03-07 14:00:09 +0100

    💄 Update components

--- src/components/react/Drawer.tsx
+++ src/components/react/Drawer.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 
 export default function Drawer() {
+	const { pathname } = window.location;
 	return (
 		<Sheet>
 			<SheetTrigger asChild className="md:hidden">
 				<MenuIcon color="white" />
 			</SheetTrigger>
 			<SheetContent>
-				<div className="flex flex-col items-end text-end text-white font-primary gap-8 px-4 py-16">
-					<a href="/garage/">Garage</a>
-					<a href="/team/">Team</a>
-					<a href="/sponsors/">Sponsors</a>
-					<a href="/contacts/">Contacts</a>
+				<div className="flex flex-col justify-between h-full px-4 py-16">
+					<div className="flex flex-col items-end text-end text-white font-primary gap-8">
+						<a href="/garage/" className={`${pathname === "/garage/" ? "text-red" : ""}`}>Garage</a>
+						<a href="/team/" className={`${pathname === "/team/" ? "text-cyan" : ""}`}>Team</a>
+						<a href="/sponsors/" className={`${pathname === "/sponsors/" ? "text-purple" : ""}`}>Sponsors</a>
+						<a href="/contacts/" className={`${pathname === "/contacts/" ? "text-orange" : ""}`}>Contacts</a>
+					</div>
+					<div className="flex justify-end text-end text-white font-primary gap-8 uppercase">
+						<a href="#">En</a>
+						<a href="#">It</a>
+					</div>
 				</div>
 			</SheetContent>
 		</Sheet>
