983aeb6f57e8e567c7928b20645aabba70b5c149
Antonio Lentini <antonio.lentini2004@gmail.com>
2025-03-24 21:18:58 +0100

    🚧 Building 'DepartmentSection'

--- src/types/members.ts
+++ src/types/members.ts
@@ -1,18 +1,19 @@
 type Department =
-	| "board"
-	| "chassis"
-	| "aerodynamics"
-	| "powertrain"
-	| "electronics"
-	| "it"
-	| "graphics"
-	| "business";
+  | "board"
+  | "chassis"
+  | "aerodynamics"
+  | "powertrain"
+  | "electronics"
+  | "it"
+  | "graphics"
+  | "business"
+  | "cost";
 
 interface Member {
-	name: string;
-	description?: string;
-	image?: string;
-	department: Department;
-	leader?: boolean;
-	linkedin?: string;
+  name: string;
+  description?: string;
+  image?: string;
+  department: Department;
+  leader?: boolean;
+  linkedin?: string;
 }
