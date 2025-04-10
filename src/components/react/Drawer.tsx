cf8d14f2bb0371cd988118af136d50b211eac831
Antonio Lentini <antonio.lentini2004@gmail.com>
2025-04-10 16:16:51 +0200

    🎨 Format code

--- src/components/react/Drawer.tsx
+++ src/components/react/Drawer.tsx
@@ -4,25 +4,25 @@ import { MenuIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 
 export default function Drawer() {
-	return (
-		<Sheet>
-			<SheetTrigger asChild className="md:hidden cursor-pointer">
-				<MenuIcon color="white" />
-			</SheetTrigger>
-			<SheetContent>
-				<div className="flex flex-col justify-between h-full px-4 py-16">
-					<div className="flex flex-col items-end text-end text-white font-primary gap-8">
-						<a href="/garage/">Garage</a>
-						<a href="/team/">Team</a>
-						<a href="/sponsors/">Sponsors</a>
-						<a href="/contacts/">Contacts</a>
-					</div>
-					<div className="flex justify-end text-end text-white font-primary gap-8 uppercase">
-						<a href="#">En</a>
-						<a href="#">It</a>
-					</div>
-				</div>
-			</SheetContent>
-		</Sheet>
-	);
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="cursor-pointer md:hidden">
+        <MenuIcon color="white" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex h-full flex-col justify-between px-4 py-16">
+          <div className="font-primary flex flex-col items-end gap-8 text-end text-white">
+            <a href="/garage/">Garage</a>
+            <a href="/team/">Team</a>
+            <a href="/sponsors/">Sponsors</a>
+            <a href="/contacts/">Contacts</a>
+          </div>
+          <div className="font-primary flex justify-end gap-8 text-end text-white uppercase">
+            <a href="#">En</a>
+            <a href="#">It</a>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
--- src/types/members.ts
+++ src/types/members.ts
@@ -1,19 +1,19 @@
 type Department =
-	| "board"
-	| "chassis"
-	| "aerodynamics"
-	| "powertrain"
-	| "electronics"
-	| "it"
-	| "graphics"
-	| "business";
+  | "board"
+  | "chassis"
+  | "aerodynamics"
+  | "powertrain"
+  | "electronics"
+  | "it"
+  | "graphics"
+  | "business";
 
 interface Member {
-	name: string;
-	description?: string;
-	image?: string;
-	department: Department;
-	leader?: boolean;
-	linkedin?: string;
-	email?: string;
+  name: string;
+  description?: string;
+  image?: string;
+  department: Department;
+  leader?: boolean;
+  linkedin?: string;
+  email?: string;
 }
