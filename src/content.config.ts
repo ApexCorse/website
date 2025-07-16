6d2246a7cbee5f78768bd8fb1963131516e88277
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-07-16 22:26:00 +0200

    chore: add dependencies for styling and configure collections
    Configure '@tailwindcss/typography' and MDX.

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob } from "astro/loaders";
+
+const updates = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/pages/updates" }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    image: z.string(),
+    date: z.date(),
+    preview: z.string(),
+  }),
+});
+
+const updatesEn = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/pages/en/updates" }),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    image: z.string(),
+    date: z.date(),
+    preview: z.string(),
+  }),
+});
+
+export const collections = { updates, updatesEn };
