2eecd1c7b95de7cbca3c242d4829776f9caba974
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-07-16 22:26:00 +0200

    chore: add dependencies for styling and configure collections
    Configure '@tailwindcss/typography' and MDX.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()]
   },
@@ -16,5 +18,8 @@ export default defineConfig({
   i18n: {
     locales: ["it", "en"],
     defaultLocale: "it"
+  },
+  server: {
+    host: true
   }
 });
